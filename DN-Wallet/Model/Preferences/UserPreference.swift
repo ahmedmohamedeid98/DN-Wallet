0de4686686b7923f3070a4f7979c9fbbb0fa9d8e
ahmedmohamedeid98 <ahmedmohamedeid98@gmail.com>
2020-04-21 14:36:01 -0700

    add alert view and userpreference classes

--- DN-Wallet/Model/Preferences/UserPreference.swift
+++ DN-Wallet/Model/Preferences/UserPreference.swift
@@ -14,4 +14,5 @@ class UserPreference {
         return .EGP
     }
     
+    
 }
--- DN-Wallet/View/Alerts/Alert.swift
+++ DN-Wallet/View/Alerts/Alert.swift
@@ -2,8 +2,45 @@
 //  Alert.swift
 //  DN-Wallet
 //
-//  Created by Mac OS on 4/21/20.
+//  Created by Ahmed Eid on 4/21/20.
 //  Copyright © 2020 DN. All rights reserved.
 //
 
-import Foundation
+class Alert {
+    
+    /// Alert to success situation show then disappear after time interval, don't wait for some task to compelete.
+    class func syncSuccessfullWith(_ msg: String, dismissAfter time: TimeInterval = 1.0, viewController vc: UIViewController) {
+        let alert = UIAlertController(title: nil, message: msg, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        vc.present(alert, animated: true, completion: nil)
+        UIView.animate(withDuration: time) {
+            alert.dismiss(animated: true, completion: nil)
+        }
+    }
+    /// Alert to success situation show then disappear after time interval, wait for some task to compelete.
+    class func asyncSuccessfullWith(_ msg: String, dismissAfter time: TimeInterval = 1.0, viewController vc: UIViewController) {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: nil, message: msg, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            vc.present(alert, animated: true, completion: nil)
+            UIView.animate(withDuration: time) {
+                alert.dismiss(animated: true, completion: nil)
+            }
+        }
+        
+    }
+    /// alert with ok action, don't wait another task to execute.
+    class func syncActionOkWith(_ title: String?, msg: String?, viewController vc: UIViewController) {
+        let alert = UIAlertController(title: title, message: msg, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        vc.present(alert, animated: true, completion: nil)
+    }
+    /// alert with ok action, wait another task to execute.
+    class func asyncActionOkWith(_ title: String?, msg: String?, viewController vc: UIViewController) {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: title, message: msg, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            vc.present(alert, animated: true, completion: nil)
+        }
+    }
+}
